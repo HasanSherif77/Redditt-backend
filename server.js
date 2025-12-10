a3695db82d9054c9579e990087e116052f01ee05
Hasan <hasan.sherif2005@gmail.com>
2025-12-10 12:49:30 +0200

    mounting routes

--- server.js
+++ server.js
@@ -10,5 +10,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Import routes
+const userRoutes = require('./users/userRoute');
+const commentRoutes = require('./comments/commentRoute');
+const postRoutes = require('./posts/postRoute');
+const messageRoutes = require('./messages/messageRoute');
+const communityRoutes = require('./communities/communityRoute');
+const notificationRoutes = require('./notifications/notificationRoute');
+
+// Mount routes
+app.use('/users', userRoutes);
+app.use('/comments', commentRoutes);
+app.use('/posts', postRoutes);
+app.use('/messages', messageRoutes);
+app.use('/communities', communityRoutes);
+app.use('/notifications', notificationRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
